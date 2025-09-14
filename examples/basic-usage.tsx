30f91bffcd6bed549677b2a6ff3a9c7f207528f5
Gurleen <grlnsngh@gmail.com>
2025-09-14 05:54:50 -0400

    refactor: remove Header component from basic usage example

--- examples/basic-usage.tsx
+++ examples/basic-usage.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Button, Header } from "briza-ui-react";
+import { Button } from "briza-ui-react";
 
 function App() {
   return (
     <div>
-      <Header user={{ name: "John Doe" }} />
       <Button primary label="Click me" onClick={() => alert("Hello!")} />
     </div>
   );
